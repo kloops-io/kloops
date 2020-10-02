621d860216faa37f86e65a1f3d2aa51a78e51df7
Charles-Edouard Breteche <charled.breteche@gmail.com>
2020-10-02 10:25:10 +0200

    added refs and status to job CRD

--- apis/build/v1alpha1/job_types.go
+++ apis/build/v1alpha1/job_types.go
@@ -21,13 +21,97 @@ import (
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
+// JobState specifies the current pipelne status
+type JobState string
+
+// Various job types.
+const (
+	// TriggeredState for pipelines that have been triggered
+	TriggeredState JobState = "triggered"
+	// PendingState pipeline is pending
+	PendingState JobState = "pending"
+	// RunningState pipeline is running
+	RunningState JobState = "running"
+	// SuccessState pipeline is successful
+	SuccessState JobState = "success"
+	// FailureState failed
+	FailureState JobState = "failure"
+	// AbortedState aborted
+	AbortedState JobState = "aborted"
+	// ErrorState means the job could not schedule (bad config, perhaps).
+	ErrorState JobState = "error"
+)
+
+// Pull describes a pull request at a particular point in time.
+type Pull struct {
+	Number int    `json:"number"`
+	Author string `json:"author"`
+	SHA    string `json:"sha"`
+	Title  string `json:"title,omitempty"`
+
+	// Ref is git ref can be checked out for a change
+	// for example,
+	// github: pull/123/head
+	// gerrit: refs/changes/00/123/1
+	Ref string `json:"ref,omitempty"`
+	// Link links to the pull request itself.
+	Link string `json:"link,omitempty"`
+	// CommitLink links to the commit identified by the SHA.
+	CommitLink string `json:"commitLink,omitempty"`
+	// AuthorLink links to the author of the pull request.
+	AuthorLink string `json:"authorLink,omitempty"`
+}
+
+// Refs describes how the repo was constructed.
+type Refs struct {
+	// Org is something like kubernetes or k8s.io
+	Org string `json:"org"`
+	// Repo is something like test-infra
+	Repo string `json:"repo"`
+	// RepoLink links to the source for Repo.
+	RepoLink string `json:"repoLink,omitempty"`
+	BaseRef  string `json:"baseRef,omitempty"`
+	BaseSHA  string `json:"baseSha,omitempty"`
+	// BaseLink is a link to the commit identified by BaseSHA.
+	BaseLink string `json:"baseLink,omitempty"`
+
+	Pulls []Pull `json:"pulls,omitempty"`
+
+	// PathAlias is the location under <root-dir>/src
+	// where this repository is cloned. If this is not
+	// set, <root-dir>/src/github.com/org/repo will be
+	// used as the default.
+	PathAlias string `json:"pathAlias,omitempty"`
+	// CloneURI is the URI that is used to clone the
+	// repository. If unset, will default to
+	// `https://github.com/org/repo.git`.
+	CloneURI string `json:"cloneUri,omitempty"`
+	// SkipSubmodules determines if submodules should be
+	// cloned when the job is run. Defaults to true.
+	SkipSubmodules bool `json:"skipSubmodules,omitempty"`
+	// CloneDepth is the depth of the clone that will be used.
+	// A depth of zero will do a full clone.
+	CloneDepth int `json:"cloneDepth,omitempty"`
+}
+
 // JobSpec defines the desired state of Job
 type JobSpec struct {
+	// Refs is the code under test, determined at runtime
+	Refs *Refs `json:"refs,omitempty"`
+
 	Resource runtime.RawExtension `json:"resource,omitempty"`
 }
 
 // JobStatus defines the observed state of Job
 type JobStatus struct {
+	// State is the full state of the job
+	State JobState `json:"state,omitempty"`
+	// ReportURL is the link that will be used in the commit status.
+	ReportURL string `json:"reportURL,omitempty"`
+	// StartTime is when the job was created.
+	StartTime metav1.Time `json:"startTime,omitempty"`
+	// CompletionTime is when the job finished reconciling and entered a terminal state.
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
 }
 
 // +kubebuilder:object:root=true
